558fa6c917a51569a5671a31d1536e8a1acb05f6
PAVAN-npx <gangstarrider9@gmail.com>
2025-03-11 10:22:14 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // const __dirname = path.resolve(); 
 const __dirname = 'https://raw.githubusercontent.com/PAVAN-npx/ecommerce/main';
-';
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/api/v1/products', productRoutes);
